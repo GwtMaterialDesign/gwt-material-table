8a32afdfbfc3fc2f8cec78dbed6ced7a578fe5c5
BenDol <dolb90@gmail.com>
2017-07-30 03:23:13 +1200

    Fix some license positions.

--- src/main/java/gwt/material/design/client/jquery/JQueryMutate.java
+++ src/main/java/gwt/material/design/client/jquery/JQueryMutate.java
@@ -1,10 +1,8 @@
-package gwt.material.design.client.jquery;
-
 /*
  * #%L
  * GwtMaterial
  * %%
- * Copyright (C) 2015 - 2016 GwtMaterialDesign
+ * Copyright (C) 2015 - 2017 GwtMaterialDesign
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +17,7 @@ package gwt.material.design.client.jquery;
  * limitations under the License.
  * #L%
  */
-
+package gwt.material.design.client.jquery;
 
 import com.google.gwt.dom.client.Element;
 import gwt.material.design.jquery.client.api.JQueryElement;
