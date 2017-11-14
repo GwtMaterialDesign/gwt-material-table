d7941a133fdaedeb2faf8e0eed9241b9db27cc08
Sašo Petrovič <saso.petrovic@gmail.com>
2017-11-14 07:29:21 +0100

    Replacing drop-down component with the input box

--- src/main/java/gwt/material/design/client/ui/pager/PageNumberSelection.java
+++ src/main/java/gwt/material/design/client/ui/pager/PageNumberSelection.java
@@ -23,9 +23,10 @@ import com.google.gwt.dom.client.Document;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.HasValue;
+
 import gwt.material.design.client.base.MaterialWidget;
 import gwt.material.design.client.base.constants.TableCssName;
-import gwt.material.design.client.ui.MaterialListValueBox;
+import gwt.material.design.client.ui.MaterialIntegerBox;
 import gwt.material.design.client.ui.html.Span;
 
 /**
@@ -35,11 +36,11 @@ import gwt.material.design.client.ui.html.Span;
  */
 public class PageNumberSelection extends MaterialWidget implements HasValue<Integer> {
 
-    private final MaterialDataPager pager;
+    private final MaterialDataPager<?> pager;
     protected Span pageLabel = new Span("Page");
-    protected MaterialListValueBox<Integer> listPages = new MaterialListValueBox<>();
+    protected MaterialIntegerBox pages = new MaterialIntegerBox();
 
-    public PageNumberSelection(MaterialDataPager pager) {
+    public PageNumberSelection(MaterialDataPager<?> pager) {
         super(Document.get().createDivElement(), TableCssName.NUM_PAGE_PANEL);
         this.pager = pager;
     }
@@ -50,51 +51,36 @@ public class PageNumberSelection extends MaterialWidget implements HasValue<Inte
 
         setGrid("s12 m4 l3");
         setOffset("l3");
-
-
-        add(listPages);
+        
+        // Label
         add(pageLabel);
-
-        registerHandler(listPages.addValueChangeHandler(event -> pager.gotoPage(event.getValue())));
-    }
-
-    /**
-     * Re populate the total number of pages
-     */
-    public void updatePageNumber(int totalRows, int limit, int currentPage) {
-        listPages.clear();
-        int pages = (pager.isExcess()) ? (totalRows / limit) + 1 : totalRows / limit;
-        for (int i = 1; i <= pages; i++) {
-            listPages.addItem(i);
-        }
-        listPages.setSelectedIndex(currentPage - 1);
+        
+        // Input
+        pages.setMin("1");
+        add(pages);
     }
 
     @Override
     public Integer getValue() {
-        return listPages.getValue();
+        return pages.getValue();
     }
 
     @Override
     public void setValue(Integer value) {
-        listPages.setValue(value);
+    	pages.setValue(value);
     }
 
     @Override
     public void setValue(Integer value, boolean fireEvents) {
-        listPages.setValue(value, fireEvents);
+    	pages.setValue(value, fireEvents);
     }
+    
+    public void updatePageNumber(int currentPage) {
+    	pages.setValue(currentPage, false);
+	}
 
     @Override
     public HandlerRegistration addValueChangeHandler(ValueChangeHandler<Integer> valueChangeHandler) {
-        return listPages.addValueChangeHandler(valueChangeHandler);
-    }
-
-    public void setSelectedIndex(int index) {
-        listPages.setSelectedIndex(index);
-    }
-
-    public int getSelectedIndex() {
-        return listPages.getSelectedIndex();
+        return pages.addValueChangeHandler(valueChangeHandler);
     }
 }
